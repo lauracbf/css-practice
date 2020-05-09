c96f7e64a4b2a2537d99688466c74e8430fd0d84
Laura <laurarroncbf@gmail.com>
2020-05-09 13:31:28 -0500

    Practica CSS

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+console.log('1');
+
+(function(){
+  'use strict';
+  document.addEventListener('DOMContentLoaded', function(){
+    
+    
+      console.log('2');
+      
+    
+    
+  });
+  
+})();
+
+console.log('3');
